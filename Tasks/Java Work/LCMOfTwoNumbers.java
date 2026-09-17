5b6b7b8d9c7edc5b758606ca5dea2783c5779c80
olaisraeltolola <olaisraelshyne@gmail.com>
2026-09-17 18:36:30 +0100

    LCM of Two Numbers

--- Tasks/Java Work/LCMOfTwoNumbers.java
+++ Tasks/Java Work/LCMOfTwoNumbers.java
@@ -10,25 +10,18 @@ int numberOne = input.nextInt();
 System.out.println("Enter the second number");
 int numberTwo = input.nextInt();
 
-int index = 2;
-int product = 1;
+int smallest = numberOne;
 
-while (numberOne != 1 && numberTwo != 1){
+if (numberOne > numberTwo)
+smallest = numberTwo;
 
-if (numberOne % index == 0 && numberTwo % index != 0)
-numberOne = numberOne / index;
-
-if (numberOne % index != 0 && numberTwo % index == 0)
-numberTwo = numberTwo / index;
-
-product = product * index;
-
-if (numberOne % index != 0 && numberTwo % index != 0)
+int index = smallest;
+while (index >= smallest){
+if (index % numberOne == 0 && index % numberTwo == 0)
+break;
+else
 index++;
-
-
-System.out.println(product);
+}
+System.out.print(index);		
 }
 }
-}
-
